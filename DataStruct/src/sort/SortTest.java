b82db46b32df4f5fdf6d61ee455a5c9fa199aa83
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2018-03-02 17:52:15 +0800

    快速排序，二分查找，归并排序

--- DataStruct/src/sort/SortTest.java
+++ DataStruct/src/sort/SortTest.java
@@ -1,5 +1,9 @@
 package sort;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * Created by gavin on 2018/1/29.
  */
@@ -132,7 +136,7 @@ public class SortTest {
         int i=beginIndex;
         int j=toIndex;
 
-        int temp=array[beginIndex];
+        int temp=array[beginIndex];//存储基准数
 
         while (i<j){
 
@@ -141,6 +145,7 @@ public class SortTest {
             while (array[j]>=temp&&i<j)
                 j--;
 
+            //从左边往右找。
             while (array[i]<=temp&& i<j)
                 i++;
 
@@ -153,13 +158,99 @@ public class SortTest {
 
         }
 
+        //最终将基数归为
         array[beginIndex]=array[i];
 
         array[i]=temp;
 
+
+
         quickSort(array,beginIndex,i-1);
 
         quickSort(array,i+1,toIndex);
+
+    }
+
+
+    @Test
+    public void testmergeSort(){
+        int []arr = {9,8,7,6,5,4,3,2,1};
+
+        int [] temp=new int[arr.length];
+
+        mergeSort(arr,0,arr.length-1,temp);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+    /****
+     * 归并排序
+     * @param arr
+     * @param left
+     * @param right
+     * @param temp   在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+     */
+    public static void mergeSort(int[] arr,int left,int right,int []temp){
+
+        if (left<right){
+
+            int mid=(left+right)>>>1;
+            mergeSort(arr,left,mid,temp);//
+            mergeSort(arr,mid+1,right,temp);//
+            merge(arr,left,mid,right,temp);
+        }
+
+    }
+
+    /***
+     * 将两个有序子数组合并操作
+     * @param arr
+     * @param left
+     * @param mid
+     * @param right
+     * @param temp
+     */
+    private static void merge(int[] arr,int left,int mid,int right,int[] temp){
+
+        int i=left;//左序列指针
+
+        int j=mid+1;//右序列指针
+
+
+        int t=0;//临时数组指针
+
+        while (i<=mid&&j<=right){
+
+            if (arr[i]<=arr[j]){
+
+                temp[t++]=arr[i++];
+            }else {
+
+                temp[t++]=arr[j++];
+            }
+        }
+
+        while (i<=mid){//将左边剩余元素填充进temp中
+
+            temp[t++]=arr[i++];
+        }
+
+        while (j<=right){//将右序列剩余元素填充进temp中
+
+            temp[t++]=arr[j++];
+        }
+
+        t=0;
+
+        //将temp中的元素全部拷贝到原数组中
+
+        while (left<=right){
+
+            arr[left++]=temp[t++];
+        }
+
 
     }
 }
